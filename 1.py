6280caa3979073d974c7f0eee4e1f0ae4b582654
ailin.hajipoor <ailin.hajipoor@gmail.com>
2023-08-31 12:57:03 +0330

    In avvali commit man ast.

---
+++ 1.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+def save():
+    f=open(f"{e_name.get()}.txt", 'w')
+    f.write(f"{e_name.get()}\n{e_surname.get()}\n{e_age.get()}")
+    f.close()    
+root = Tk()
+root.geometry("600x300")
+root.resizable(1,1) # کم کاربرد
+root.title("preject 1")
+root.config(bg='sky blue')
+l_name=Label(root, text="Enter your name: ")
+l_name.pack()
+e_name = Entry(root)
+e_name.pack()
+l_surname=Label(root, text="Enter your surname: ")
+l_surname.pack()
+e_surname = Entry(root)
+e_surname.pack()
+l_age=Label(root, text="Enter your age: ")
+l_age.pack()
+e_age = Entry(root)
+e_age.pack()
+btn_exit = Button(root, text='Exit', command=exit)
+btn_exit.pack()
+btn_save = Button(root, text='Save', command=save)
+btn_save.pack()
+mainloop()
